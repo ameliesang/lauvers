c589019f5fcde6014998d83801e789962b5cf8d3
ameliesang <amelie.sang@edu.devinci.fr>
2025-02-09 23:01:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  var swiper = new Swiper(".mySwiper2", {
+    pagination: {
+      el: ".swiper2-pagination",
+      dynamicBullets: true,
+    },
+  });
